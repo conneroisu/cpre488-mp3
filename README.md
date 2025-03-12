94377e3504eb4718c5f3748457fbb14dc4882961
connerohnesorge <conneroisu@outlook.com>
2025-03-12 11:33:54 -0500

    batman

---
+++ README.md
@@ -0,0 +1,9 @@
+# cpre488-mp3
+
+- [ ] Explain how this Make process was configured to appropriately use a cross-compiler targeting the ARM architecture.
+- [ ] Explanatory annotation of the boot messages that print as PetaLinux starts up. 
+- [ ] Include the kernel messages that result from the plugging in the of the usb missle launcher.
+- [ ] Describe changes made to the usb driver `usb-skeletion.c`
+- [ ] Describe the operation of the `launcher_fire.c` file.
+- [ ] Describe algorithm used to detect the target.
+
---
+++ flake.nix
@@ -0,0 +1,97 @@
+{
+  description = "Optimized primitives for collective multi-GPU communication in Zig";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.systems.follows = "systems";
+
+    systems.url = "github:nix-systems/default";
+
+    zig = {
+      url = "github:mitchellh/zig-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+    zls-overlay.url = "github:zigtools/zls";
+
+    zig2nix = {
+      url = "github:jcollie/zig2nix?ref=c311d8e77a6ee0d995f40a6e10a89a3a4ab04f9a";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  nixConfig = {
+    extra-substituters = ''
+      https://cache.nixos.org
+      https://nix-community.cachix.org
+      https://devenv.cachix.org
+    '';
+    extra-trusted-public-keys = ''
+      cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
+      nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
+      devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+    '';
+    extra-experimental-features = "nix-command flakes";
+  };
+
+  outputs = inputs @ {flake-utils, ...}:
+    flake-utils.lib.eachSystem [
+      "x86_64-linux"
+      "i686-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ] (system: let
+      #
+      zigpkgs = inputs.zig.packages.${system};
+      overlays = [
+        (final: prev: {
+          inherit zigpkgs;
+        })
+      ];
+      zig = zigpkgs.master;
+      zls = inputs.zls-overlay.packages.x86_64-linux.zls.overrideAttrs (old: {
+        nativeBuildInputs = [zig];
+      });
+      #
+      pkgs = import inputs.nixpkgs {inherit system overlays;};
+      #
+      script = pkgs.writeShellScriptBin;
+    in {
+      devShells.default = pkgs.mkShell {
+        shellHook = ''
+          export REPO_ROOT=$(git rev-parse --show-toplevel)
+        '';
+        packages = with pkgs; [
+          alejandra
+          nixd
+          zig
+          zls
+          clang-tools
+
+          (script "dx" ''
+            $EDITOR $REPO_ROOT/flake.nix
+          '')
+          (script "build" ''
+            nix build .#packages.x86_64-linux.conneroh
+          '')
+          (script "format" ''
+            export REPO_ROOT=$(git rev-parse --show-toplevel) # needed
+
+            # format all files with clang-format
+            find $REPO_ROOT -type f -name '*.c' -exec clang-format -i --verbose {} \;
+          '')
+        ];
+      };
+    });
+}
