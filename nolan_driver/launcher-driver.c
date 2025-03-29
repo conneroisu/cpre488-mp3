61c4069091b672d9cca60c30add039a4c99154a2
NolanEastburn <nolieman1@gmail.com>
2025-03-29 17:46:05 -0500

    -Moved interator out since petalinux is dumb

--- nolan_driver/launcher-driver.c
+++ nolan_driver/launcher-driver.c
@@ -238,7 +238,8 @@ static ssize_t miss_launch_write(struct file *file, const char *user_buffer,
 
     // Zeroize the newly allocated space.
     // memset(buf, 0, writesize);
-    for(int i = 0; i < writesize; ++i)
+    int i = 0;
+    for(i = 0; i < writesize; ++i)
     {
         buf[i] = (char) 0;
     }
