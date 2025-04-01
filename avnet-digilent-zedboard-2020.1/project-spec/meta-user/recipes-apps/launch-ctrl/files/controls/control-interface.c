121809c63d25a1614d29b3b3a9bdfe3098b76f97
Nolan Eastburn <neastbur@iastate.edu>
2025-03-31 20:19:41 -0500

    -Added launch-ctrl app, which is from the launcher-fire-buttons.c file -Compiled

---
+++ avnet-digilent-zedboard-2020.1/project-spec/meta-user/recipes-apps/launch-ctrl/files/controls/control-interface.c
@@ -0,0 +1,58 @@
+#include "control-interface.h"
+#define DREF_REG(reg_addr, offset) (volatile uint32_t*) ((uint32_t))
+
+gpio_addr_maps_t init_interface()
+{
+	long page_size = sysconf(_SC_PAGE_SIZE);
+
+	// Setup GPIO mapping using mmap()
+	gpio_addr_maps_t map;
+
+	// Open memory device
+	int mem = open(MEM_DEVICE, O_RDWR);
+
+	if(mem < 0)
+	{
+		printf("ERROR: Could not open %s\n", MEM_DEVICE);
+		return ERROR_GPIO_ADDR_MAPS;
+	}
+
+	map.button_addr = mmap(NULL, REG_SIZE_BYTES * BUTTON_REG_COUNT, PROT_READ | PROT_WRITE, MAP_PRIVATE, mem, BUTTON_BASE_ADDR / page_size);
+
+	if(map.button_addr < 0)
+	{
+		printf("ERROR: Could not get mapping to buttons!\n");
+		return ERROR_GPIO_ADDR_MAPS;
+	}
+
+	map.sw_addr = mmap(NULL, REG_SIZE_BYTES * SWITCH_REG_COUNT, PROT_READ | PROT_WRITE, MAP_PRIVATE, mem, SWITCH_BASE_ADDR / page_size);
+
+	if(map.sw_addr < 0)
+	{
+		printf("ERROR: Could not get mapping to switches!\n");
+		return ERROR_GPIO_ADDR_MAPS;
+	}
+
+	close(mem);
+
+	// Since buttons and switches are inputs, set TRI reg to 0x1.
+	map.button_addr[1] = 0x1;
+	map.sw_addr[1] = 0x1;
+
+	return map;
+}
+
+uint32_t get_button_states(gpio_addr_maps_t map)
+{
+	return map.button_addr[0];
+}
+
+uint32_t get_switch_states(gpio_addr_maps_t map)
+{
+	return map.sw_addr[0];
+}
+
+int button_pressed(t_buttons button, uint32_t state)
+{
+	return (state & (uint32_t) button) > 0;
+}
---
+++ avnet-digilent-zedboard-2020.1/project-spec/meta-user/recipes-apps/launch-ctrl/launch-ctrl.bb
@@ -0,0 +1,26 @@
+#
+# This file is the launch-ctrl recipe.
+#
+
+SUMMARY = "Simple launch-ctrl application"
+SECTION = "PETALINUX/apps"
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
+
+SRC_URI = "file://launch-ctrl.c \
+	   file://Makefile \
+	   file://launcher-commands.h \
+	   file://controls/control-interface.c \
+	   file://controls/control-interface.h \
+		  "
+
+S = "${WORKDIR}"
+
+do_compile() {
+	     oe_runmake
+}
+
+do_install() {
+	     install -d ${D}${bindir}
+	     install -m 0755 launch-ctrl ${D}${bindir}
+}
