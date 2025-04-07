4b4fb02db402cba153a5ae67ad9288111294361e
jxie9 <87154119+jxie9@users.noreply.github.com>
2025-04-06 20:48:40 -0500

    Update camera_app.h

--- docs/camera_app.h
+++ docs/camera_app.h
@@ -18,28 +18,136 @@
 #ifndef __CAMERA_APP_H__
 #define __CAMERA_APP_H__
 
-
 #include <xparameters.h>
 #include <xbasic_types.h>
 #include <stdlib.h>
-//#include <xil_printf.h>
+#include <xil_printf.h>
 //#include <sleep.h>
 #include "fmc_iic.h"
 #include "fmc_ipmi.h"
 #include "fmc_imageon.h"
 #include "onsemi_vita_sw.h"
-#include "xv_demosaic.h"   // Uncomment when using Demosaic IP core
-#include "xvprocss.h"      // Uncomment when using Video Processing Subsystem IP cores
+//#include "xv_demosaic.h"   // Uncomment when using Demosaic IP core
+//#include "xvprocss.h"      // Uncomment when using Video Processing Subsystem IP cores
 #include "xvtc.h"
 #include "xaxivdma.h"
 //#include "xtpg_app.h"
-
 
 // Constants for library code
 #define ZED_FMC_IMAGEON_GETTING_STARTED_HW
 #define ADV7511_ADDR   0x72
 
 
+/// Taken from xvprocss.h
+//typedef struct
+//{
+//  u16 DeviceId;	         /**< DeviceId is the unique ID  of the device */
+//  UINTPTR BaseAddress;   /**< BaseAddress is the physical base address of the
+//                              subsystem address range */
+//  UINTPTR HighAddress;   /**< HighAddress is the physical MAX address of the
+//                              subsystem address range */
+//  u8 Topology;           /**< Subsystem configuration mode */
+//  u8 PixPerClock;        /**< Number of Pixels Per Clock processed by Subsystem */
+//  u16 ColorDepth;        /**< Processing precision of the data pipe */
+//  u16 NumVidComponents;  /**< Number of Video Components */
+//  u16 MaxWidth;          /**< Maximum cols supported by subsystem instance */
+//  u16 MaxHeight;         /**< Maximum rows supported by subsystem instance */
+//  u16 HasMADI;           /**< Motion Adaptive Deinterlacer available flag */
+//  XSubCore RstAximm;     /**< Axi MM reset network instance configuration */
+//  XSubCore RstAxis;      /**< Axi stream reset network instance configuration */
+//  XSubCore Vdma;         /**< Sub-core instance configuration */
+//  XSubCore Router;       /**< Sub-core instance configuration */
+//  XSubCore Csc;          /**< Sub-core instance configuration */
+//  XSubCore Deint;        /**< Sub-core instance configuration */
+//  XSubCore HCrsmplr;     /**< Sub-core instance configuration */
+//  XSubCore Hscale;       /**< Sub-core instance configuration */
+//  XSubCore Lbox;         /**< Sub-core instance configuration */
+//  XSubCore VCrsmplrIn;   /**< Sub-core instance configuration */
+//  XSubCore VCrsmplrOut;  /**< Sub-core instance configuration */
+//  XSubCore Vscale;       /**< Sub-core instance configuration */
+//} XVprocSs_Config;
+//
+///**
+// * Video Processing Subsystem context scratch pad memory.
+// * This contains internal flags, state variables, routing table
+// * and other meta-data required by the subsystem. Each instance
+// * of the subsystem will have its own context data memory
+// */
+//typedef struct
+//{
+//  XVidC_VideoWindow RdWindow; /**< window for Zoom/Pip feature support */
+//  XVidC_VideoWindow WrWindow; /**< window for Zoom/Pip feature support */
+//
+//  UINTPTR DeintBufAddr;       /**< Deinterlacer field buffer Addr. in DDR */
+//  u8 PixelWidthInBits;        /**< Number of bits required to store 1 pixel */
+//
+//  u8 RtngTable[XVPROCSS_SUBCORE_MAX]; /**< Storage for computed routing map */
+//  u8 StartCore[XVPROCSS_SUBCORE_MAX]; /**< Enable flag to start sub-core */
+//  u8 RtrNumCores;             /**< Number of sub-cores in routing map */
+//  u8 ScaleMode;               /**< Stored computed scaling mode - UP/DN/1:1 */
+//  u8 ZoomEn;                  /**< Flag to store Zoom feature state */
+//  u8 PipEn;                   /**< Flag to store PIP feature state */
+//  u16 VidInWidth;             /**< Input H Active */
+//  u16 VidInHeight;            /**< Input V Active */
+//  u16 PixelHStepSize;         /**< Increment step size for Pip/Zoom window */
+//  XVidC_ColorFormat StrmCformat; /**< processing pipe color format */
+//  XVidC_ColorFormat CscIn;    /**< CSC core input color format */
+//  XVidC_ColorFormat CscOut;   /**< CSC core output color format */
+//  XVidC_ColorFormat HcrIn;    /**< horiz. cresmplr core input color format */
+//  XVidC_ColorFormat HcrOut;   /**< horiz. cresmplr core output color format */
+//  XLboxColorId LboxBkgndColor; /**< Lbox background color */
+//}XVprocSs_ContextData;
+//
+//typedef struct
+//{
+//  XVprocSs_Config Config;	         /**< Hardware configuration */
+//  u32 IsReady;		                 /**< Device and the driver instance are
+//                                       initialized */
+////  XAxis_Switch *RouterPtr;           /**< handle to sub-core driver instance */
+////  XGpio *RstAxisPtr;                 /**< handle to sub-core driver instance */
+////  XGpio *RstAximmPtr;                /**< handle to sub-core driver instance */
+//
+////  XV_Hcresampler_l2 *HcrsmplrPtr;    /**< handle to sub-core driver instance */
+////  XV_Vcresampler_l2 *VcrsmplrInPtr;  /**< handle to sub-core driver instance */
+////  XV_Vcresampler_l2 *VcrsmplrOutPtr; /**< handle to sub-core driver instance */
+////  XV_Vscaler_l2 *VscalerPtr;         /**< handle to sub-core driver instance */
+////  XV_Hscaler_l2 *HscalerPtr;         /**< handle to sub-core driver instance */
+//  XAxiVdma *VdmaPtr;                 /**< handle to sub-core driver instance */
+////  XV_Lbox_l2 *LboxPtr;               /**< handle to sub-core driver instance */
+//  XV_Csc_l2 *CscPtr;                 /**< handle to sub-core driver instance */
+////  XV_Deint_l2 *DeintPtr;             /**< handle to sub-core driver instance */
+//
+//  //I/O Streams
+//  XVidC_VideoStream VidIn;           /**< Input  AXIS configuration */
+//  XVidC_VideoStream VidOut;          /**< Output AXIS configuration */
+//
+//  XVprocSs_ContextData CtxtData;     /**< Internal Scratch pad memory for subsystem
+//                                         instance */
+//  UINTPTR FrameBufBaseaddr;          /**< Base address for frame buffer storage */
+//
+////  XVidC_DelayHandler UsrDelayUs;     /**< custom user function for delay/sleep */
+//  void *UsrTmrPtr;                   /**< handle to timer instance used by user
+//                                         delay function */
+//
+//#ifdef XV_VPROCSS_LOG_ENABLE
+//  XVprocSs_Log Log;                  /**< A log of events. */
+//#endif
+//} XVprocSs;
+//
+//
+///**
+// * Sub-Core Configuration Table
+// */
+//typedef struct
+//{
+//  u16 IsPresent;  /**< Flag to indicate if sub-core is present in the design*/
+//  u16 DeviceId;   /**< Device ID of the sub-core */
+//  u32 AddrOffset; /**< sub-core offset from subsystem base address */
+//}XSubCore;
+
+
+
+/////// Begin original camera_app.h
 // This structure contains the configuration context for the
 // camera peripherals
 struct struct_camera_config_t {
@@ -171,6 +279,7 @@ int vfb_tx_stop ( XAxiVdma *pAxiVdma );
 int vfb_dump_registers( XAxiVdma *pAxiVdma);
 int vfb_check_errors( XAxiVdma *pAxiVdma, u8 bClearErrors );
 
-
-
 #endif // __CAMERA_APP_H__
+
+
+
